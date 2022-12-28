e11a952a93b165d5f7d3343f30770b0a0e0cdde4
ALEXANDREGBAMELE <112797022+ALEXANDREGBAMELE@users.noreply.github.com>
2022-12-28 10:26:51 +0100

    daily challenge

---
+++ script.js
@@ -0,0 +1,16 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+console.log(fruits[0]);
+fruits.splice(1, 1);
+console.log(fruits);
+//delete fruits[0]
+console.log(fruits);
+
+//Tri du tableau
+fruits.sort();
+console.log(fruits);
+fruits.push("Kiwi");
+fruits.concat()
+//tri par ordre alphabetique
+console.log(fruits.sort())
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits[1][1].shift())
