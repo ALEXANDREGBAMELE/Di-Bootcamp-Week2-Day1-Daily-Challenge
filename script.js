28a89321b695bc10b0d03c1545fe056a809d4674
ALEXANDREGBAMELE <112797022+ALEXANDREGBAMELE@users.noreply.github.com>
2022-12-29 08:23:53 +0100

    daily challenge

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
-console.log(fruits[0]);
-fruits.splice(1, 1);
-console.log(fruits);
-//delete fruits[0]
-console.log(fruits);
+    let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+    console.log(fruits[0]);
+    fruits.splice(0, 1);
+    console.log(fruits);
 
-//Tri du tableau
-fruits.sort();
-console.log(fruits);
-fruits.push("Kiwi");
-fruits.concat()
-    //AUTRE METHODE
-fruits = fruits.filter((value)=> value != "Apples");
-fruits.reverse
-//tri par ordre alphabetique
-console.log(fruits.sort())
-const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-console.log(moreFruits[1][1].shift());
+    //Tri du tableau
+    fruits.sort();
+    console.log(fruits);
+    
+    fruits.push("Kiwi");
+    console.log(fruits);
+    //fruits.concat();
+        //AUTRE METHODE
+    fruits = fruits.filter((value)=> value != "Apples");
+    fruits.reverse()
+    console.log(fruits.sort())
+    const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+    console.log(moreFruits[1][1].shift());
+
+    //tri par ordre alphabetique inverse
+    console.log(fruits.reverse());
